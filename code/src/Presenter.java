c3d62d128d0d77aa4ce29db5063ab50d670debc1
Florian Reimair <florian.reimair@itdevas.at>
2012-11-23 15:34:24 +0100

    open dialog is in place

--- code/src/Presenter.java
+++ code/src/Presenter.java
@@ -1,7 +1,18 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
+
 
 public class Presenter {
 	public static void main(String[] args) {
-		PresenterControl myControl = new PresenterControl();
-		myControl.open();
+		// PresenterControl myControl = new PresenterControl();
+		// myControl.open();
+
+		Display display = new Display();
+		FileDialog fileDialog = new FileDialog(new Shell(display), SWT.OPEN);
+		fileDialog
+				.setFilterExtensions(new String[] { "*.pdf", "*.xml", "*.jpg" });
+		fileDialog.open();
 	}
 }
